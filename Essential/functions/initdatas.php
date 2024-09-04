af0d87e1aa3e8f006f4e7d2be854f161c3cf79fc
Anson Cheng <Anson.12666@icloud.com>
2024-09-04 19:59:52 +0800

    chore: change csv source to db

--- Essential/functions/initdatas.php
+++ Essential/functions/initdatas.php
@@ -1,4 +1,7 @@
 <?php
+include_once(__DIR__ . '/loadenv.php');
+
+
 if (!isset($initdataitems))
     $initdataitems = array(
         "Route" => true,
@@ -11,66 +14,98 @@ if (!isset($initdataitems))
         "Websites" => true
     );
 
-if ($initdataitems["Route"] === true)
-    foreach (csv_to_array(__DIR__ . "/../../Data/Route") as $busno) {
-        $bus[$busno[0]]["schedule"] = array($busno[1], $busno[2], $busno[3], $busno[4], $busno[5], $busno[6]);
-        foreach (array_filter(array_slice($busno, 7)) as $key => $value) {
-            $statnm = strstr($value, '|', true) ?: $value;
-            $attr = substr(strstr($value, '|', false), 1) ?: "NULL";
-            $time = substr(strstr($attr, '|', false), 1) ?: "0";
-            $attr = strstr($attr, '|', true) ?: "NULL";
-            $bus[$busno[0]]["stations"]["name"][] = $statnm;
-            $bus[$busno[0]]["stations"]["attr"][] = $attr;
-            $bus[$busno[0]]["stations"]["time"][] = floatval($time);
+$conn = new mysqli(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'));
+if ($conn->connect_error)
+    die("Connection failed: " . $conn->connect_error);
+
+
+if ($initdataitems["Route"] === true) {
+    $stmt = $conn->prepare("SELECT * FROM Route");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $busno = $row['BUSNO'];
+        $bus[$busno]["schedule"] = array($row['StartTime'], $row['EndTime'], $row['Period'], $row['Days'], $row['Weekdays'], $row['Warning']);
+        $stations = json_decode($row['Route'], true);
+        foreach ($stations as $station) {
+            $bus[$busno]["stations"]["name"][] = $station[0];
+            $bus[$busno]["stations"]["attr"][] = $station[1] ?? "NULL";
+            $bus[$busno]["stations"]["time"][] = floatval($station[2] ?? "0");
         }
     }
+}
 
-if (
-    isset($initdataitems["Translate"]) &&
-    $initdataitems["Translate"] === true
-)
-    foreach (array_slice(csv_to_array(__DIR__ . "/../../Data/Translate"), 1) as $row) {
-        if ($row[0] !== "" && substr($row[0], 0, 2) !== "//") {
-            $translation[$row[0]] = array($row[2], $row[3]);
-            $translation[$row[0]][] = $row[1];
+if (isset($initdataitems["Translate"]) && $initdataitems["Translate"] === true) {
+    $stmt = $conn->prepare("SELECT * FROM translateroute UNION SELECT * FROM translatebuilding");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $translation[$row['Code']] = array($row['ENG'], $row['中文']);
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM translatewebsite UNION SELECT * FROM translatebuilding");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $translation[$row['Code']] = array($row['ENG'], $row['中文']);
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM translatebuilding UNION SELECT * FROM translatebuilding");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $translation[$row['Code']] = array($row['ENG'], $row['中文']);
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM translateattribute UNION SELECT * FROM translatebuilding");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $translation[$row['Code']] = array($row['ENG'], $row['中文']);
+    }
+}
+
+if (isset($initdataitems["Station"]) && $initdataitems["Station"] === true) {
+    $stmt = $conn->prepare("SELECT * FROM station");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $station[$row['最近之車站']][] = $row['建築物'];
+    }
+}
+
+if (isset($initdataitems["Notice"]) && $initdataitems["Notice"] === true) {
+    $stmt = $conn->prepare("SELECT * FROM notice");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $index = 0;
+    while ($row = $result->fetch_assoc()) {
+        $notice[$index]["content"] = [$row['CHINESE'], $row['ENGLISH']];
+        $notice[$index]["pref"]["type"] = $row['Type'];
+        $index++;
+    }
+}
+
+if (isset($initdataitems["GPS"]) && $initdataitems["GPS"] === true) {
+    $stmt = $conn->prepare("SELECT * FROM gps");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $GPS[$row['Location']]["Lat"] = $row['Lat'];
+        $GPS[$row['Location']]["Lng"] = $row['Lng'];
+    }
+}
+
+if (isset($initdataitems["Websites"]) && $initdataitems["Websites"] === true) {
+    $stmt = $conn->prepare("SELECT * FROM website");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        if ($row['URL'] !== "" && substr($row['URL'], 0, 2) !== "//") {
+            $WebsiteLinks[] = [[$row['中文'], $row['英文']], $row['URL']];
         }
     }
-
-if (
-    isset($initdataitems["Station"]) &&
-    $initdataitems["Station"] === true
-)
-    foreach (array_slice(csv_to_array(__DIR__ . "/../../Data/Station"), 1) as $row) {
-        $station[$row[1]][] = $row[0];
-    }
-
-if (
-    isset($initdataitems["Notice"]) &&
-    $initdataitems["Notice"] === true
-)
-    foreach (array_slice(csv_to_array(__DIR__ . "/../../Data/Notice"), 1) as $index => $row) {
-        $notice[$index]["content"] = array_slice($row, 1);
-        $notice[$index]["pref"]["type"] = $row[0];
-    }
-
-if (
-    isset($initdataitems["GPS"]) &&
-    $initdataitems["GPS"] === true
-)
-    foreach (array_slice(csv_to_array(__DIR__ . "/../../Data/GPS"), 1) as $row) {
-        $GPS[$row[0]]["Lat"] = $row[1];
-        $GPS[$row[0]]["Lng"] = $row[2];
-    }
-
-if (
-    isset($initdataitems["Websites"]) &&
-    $initdataitems["Websites"] === true
-)
-    foreach (array_slice(csv_to_array("Data/Websites"), 1) as $row) {
-        if ($row[2] !== "" && substr($row[2], 0, 2) !== "//") {
-            $WebsiteLinks[] = [[$row[0], $row[1]], $row[2]];
-        }
-    }
+}
 
 
 //Load building name to js
@@ -82,7 +117,7 @@ if (
 
     foreach ($translation as $name => $value)
         if (end($value))
-            echo 'Translation["' . $name . '"] = "' . $value[$lang] . '";';
+            echo 'Translation["' . $name . '"] = "' . $value[$lang] . '";' . "\n";
 
     echo "</script>";
 }
@@ -91,14 +126,17 @@ if (
     isset($initdataitems["JSGPS"]) &&
     $initdataitems["JSGPS"] === true
 ) {
-    echo "<script>const GPSdata = [";
+    echo "\n<script>const GPSdata = [";
+
+
 
     foreach ($GPS as $name => $location) {
         $nameattr = substr(strstr($name, '|', false), 1) ?: "";
         $name = strstr($name, '|', true) ?: $name;
+
         try {
             if ($location["Lat"] . $location["Lng"])
-                echo "{ \"location\": \"" . $translation[$name][$lang] . "\",  \"lat\": \"" . $location["Lat"] . "\",  \"lng\": \"" . $location["Lng"] . "\", \"attr\" : \"" . $translation[$nameattr][$lang] . "\" , \"code\" : \"" . $name . "\"  }, ";
+                echo "{ \"location\": \"" . $translation[$name][$lang] . "\",  \"lat\": \"" . $location["Lat"] . "\",  \"lng\": \"" . $location["Lng"] . "\", \"attr\" : \"" . (isset($translation[$nameattr][$lang]) ?? $translation[$nameattr][$lang]) . "\" , \"code\" : \"" . $name . "\"  }, \n";
         } catch (Exception $e) {
         }
     }
