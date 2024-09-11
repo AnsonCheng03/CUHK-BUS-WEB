65525b0f0066f1eacb1f3bd74942214afc937327
Anson Cheng <Anson.12666@icloud.com>
2024-09-11 18:49:11 +0800

    chore: create PWA template

--- cu-bus/vite.config.ts
+++ cu-bus/vite.config.ts
@@ -1,18 +1,17 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy(), VitePWA({ registerType: "autoUpdate" })],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
