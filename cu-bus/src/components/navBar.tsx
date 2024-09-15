851b92abb025c222349e5384155306aa19103123
Anson Cheng <Anson.12666@icloud.com>
2024-09-16 00:14:09 +0800

    chore: refactor navbar

--- cu-bus/src/components/navBar.tsx
+++ cu-bus/src/components/navBar.tsx
@@ -1,7 +1,7 @@
 import "./navBar.css";
 
 import { IonIcon, IonItem } from "@ionic/react";
-import React, { useEffect } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import {
   homeOutline,
@@ -13,74 +13,32 @@ import { useLocation } from "react-router-dom";
 
 const NavBar: React.FC = () => {
   const { t } = useTranslation("global");
-
   const location = useLocation();
 
-  useEffect(() => {
-    console.log("Current URL:", location.pathname);
-  }, [location]);
+  const navItem = (icon: string, text: string, link: string) => {
+    return (
+      <li>
+        <IonItem routerLink={link} routerDirection="none" detail={false}>
+          <div
+            className={`navLink ${location.pathname === link ? "active" : ""}`}
+          >
+            <IonIcon icon={icon} />
+            <p className="navtext">{text}</p>
+          </div>
+        </IonItem>
+      </li>
+    );
+  };
 
   return (
     <nav>
       <h1>{t("WEB-Title")}</h1>
       <div className="navFunctions">
         <ul>
-          <li>
-            <IonItem
-              routerLink="/realtime"
-              routerDirection="none"
-              detail={false}
-            >
-              <div
-                className={`navLink ${
-                  location.pathname === "/realtime" ? "active" : ""
-                }`}
-              >
-                <IonIcon icon={homeOutline} />
-                <p className="navtext">{t("NAV-Home")}</p>
-              </div>
-            </IonItem>
-          </li>
-          <li>
-            <IonItem routerLink="/route" routerDirection="none" detail={false}>
-              <div
-                className={`navLink ${
-                  location.pathname === "/route" ? "active" : ""
-                }`}
-              >
-                <IonIcon icon={searchOutline} />
-                <p className="navtext">{t("NAV-StationSearch")}</p>
-              </div>
-            </IonItem>
-          </li>
-          <li>
-            <IonItem routerLink="/info" routerDirection="none" detail={false}>
-              <div
-                className={`navLink ${
-                  location.pathname === "/info" ? "active" : ""
-                }`}
-              >
-                <IonIcon icon={informationOutline} />
-                <p className="navtext">{t("NAV-Info")}</p>
-              </div>
-            </IonItem>
-          </li>
-          <li>
-            <IonItem
-              routerLink="/settings"
-              routerDirection="none"
-              detail={false}
-            >
-              <div
-                className={`navLink ${
-                  location.pathname === "/settings" ? "active" : ""
-                }`}
-              >
-                <IonIcon icon={settingsOutline} />
-                <p className="navtext">{t("NAV-Settings")}</p>
-              </div>
-            </IonItem>
-          </li>
+          {navItem(homeOutline, t("NAV-Home"), "/realtime")}
+          {navItem(searchOutline, t("NAV-StationSearch"), "/route")}
+          {navItem(informationOutline, t("NAV-Info"), "/info")}
+          {navItem(settingsOutline, t("NAV-Settings"), "/settings")}
         </ul>
       </div>
     </nav>
