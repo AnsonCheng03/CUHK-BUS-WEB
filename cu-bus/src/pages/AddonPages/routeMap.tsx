670b7b3ecf5af7c985d7ffd819f307d7fce2ad2f
Anson Cheng <Anson.12666@icloud.com>
2024-10-20 21:28:07 +0800

    chore: change route map position

--- cu-bus/src/pages/AddonPages/routeMap.tsx
+++ cu-bus/src/pages/AddonPages/routeMap.tsx
@@ -5,7 +5,13 @@ import "./routeMap.css";
 const BusMap = () => {
   return (
     <div className="busMapContainer">
-      <TransformWrapper initialScale={1} wheel={{ step: 50 }} maxScale={100}>
+      <TransformWrapper
+        initialScale={3}
+        wheel={{ step: 50 }}
+        maxScale={100}
+        initialPositionX={-350}
+        initialPositionY={-350}
+      >
         <TransformComponent>
           <img src={busMapImage} alt="Bus Map" />
         </TransformComponent>
